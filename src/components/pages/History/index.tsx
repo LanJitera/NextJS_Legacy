8044ed9b87cdd38b599b51187c2e74264acdad16
Chicken2k <dangconglan2k@gmail.com>
2023-06-22 23:42:03 +0700

    lấy jitera về

--- src/components/pages/History/index.tsx
+++ src/components/pages/History/index.tsx
@@ -3,13 +3,11 @@ import React, { useCallback, useMemo, useEffect } from "react";
 import { DefaultPageProps } from "@interfaces/page";
 import get from "lodash/get";
 import HeroSection from "@components/molecules/HeroSection";
-import Image from "next/future/image";
 import FooterSection from "@components/molecules/FooterSection";
 import { useTranslation } from "next-i18next";
-import assets from "@assets/index";
-import CardItem from "@components/molecules/CardItem";
+import BlogCard from "@components/molecules/BlogCard";
 import { useAuthenticationService } from "@services/authentication";
-import { usePartyService } from "@services/party";
+import { usePartybookingService } from "@services/partybooking";
 import { Page, Box, Text, List } from "@jitera/jitera-web-ui-library";
 import styles from "./styles.module.css";
 type HistoryPageProps = DefaultPageProps & {
@@ -19,15 +17,13 @@ type HistoryPageProps = DefaultPageProps & {
 function HistoryPage(props: HistoryPageProps): JSX.Element {
   const { t } = useTranslation("web");
   const authenticationService = useAuthenticationService();
-  const authenticatedDataValue =
-    authenticationService.useAuthenticatedData("authenticatedData");
-  const partyService = usePartyService();
-  const getApiPartiesBookingHistoryInstance =
-    partyService.useGetApiPartiesBookingHistory();
-  const getApiPartiesBookingHistoryResult =
-    getApiPartiesBookingHistoryInstance.useQuery({
-      useid: get(authenticatedDataValue, "id"),
-    });
+  const authenticatedDataValue = authenticationService.useAuthenticatedData("authenticatedData");
+  const partybookingService = usePartybookingService();
+  const getApiPartybookingsInstance = partybookingService.useGetApiPartybookings();
+  const getApiPartybookingsResult = getApiPartybookingsInstance.useQuery({
+    partybookings: { user_id: get(authenticatedDataValue, "id") },
+  });
+
   return (
     <Page className={styles.page_container}>
       <HeroSection className={styles.herosection_1} />
@@ -39,42 +35,32 @@ function HistoryPage(props: HistoryPageProps): JSX.Element {
                 {t("history.text_5")}
               </Text>
             </Box>
-              {/* dataSource={undefined}
+          </Box>
+          <Box className={styles.box_5}>
+            <List
+              dataSource={undefined}
               rowKey={useCallback(
                 (item: Record<string, any>) => `${item.id}_${item.created_at}`,
                 []
               )}
-              grid={{ gutter: 0, xl: 12 }}
+              grid={{ gutter: 48, xl: 4, md: 2, xs: 1 }}
               renderItem={useCallback(
                 (item: any) => (
-                  <CardItem
-                    nameParty={get(
-                      getApiPartiesBookingHistoryResult,
-                      "data.parties.nameparty"
-                    )}
-                    partystarttime={get(
-                      getApiPartiesBookingHistoryResult,
-                      "data.parties.partystarttime"
-                    )}
-                    partyLocation={get(
-                      getApiPartiesBookingHistoryResult,
-                      "data.parties.partylocation"
-                    )}
-                    decribe={get(
-                      getApiPartiesBookingHistoryResult,
-                      "data.parties.describe"
-                    )}
-                    img={get(
-                      getApiPartiesBookingHistoryResult,
-                      "data.parties.nameparty"
-                    )}
-                    label={"Huỷ đặt"}
-                    Id={""}
+                  <BlogCard
+                    nameParty={get(getApiPartybookingsResult, "data.party.nameparty")}
+                    partystarttime={get(getApiPartybookingsResult, "data.party.partystarttime")}
+                    partyLocation={get(getApiPartybookingsResult, "data.party.partylocation")}
+                    decribe={get(getApiPartybookingsResult, "data.party.describe")}
+                    img={get(getApiPartybookingsResult, "data.party.nameparty")}
+                    label={"Hủy đặt vé "}
+                    IdPartyBooking={get(getApiPartybookingsResult, "data.partybookings.id")}
+                    numberofpeople={get(getApiPartybookingsResult, "data.party.numberofpeople")}
+                    IdParty={get(getApiPartybookingsResult, "data.party.id")}
                   />
                 ),
                 []
               )}
-            /> */}
+            />
           </Box>
         </Box>
       </Box>
