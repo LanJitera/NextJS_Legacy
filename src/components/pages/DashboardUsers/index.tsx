e871839dc6b373a89f174bf8dbe7a2c2b3ba3309
Chicken2k <dangconglan2k@gmail.com>
2023-06-28 15:58:50 +0700

    update

--- src/components/pages/DashboardUsers/index.tsx
+++ src/components/pages/DashboardUsers/index.tsx
@@ -4,9 +4,15 @@ import { DefaultPageProps } from "@interfaces/page";
 import get from "lodash/get";
 import DashboardNavbar from "@components/molecules/DashboardNavbar";
 import DashboardSidebar from "@components/molecules/DashboardSidebar";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { ArrayElement } from "@utils/array";
 import DashboardFooter from "@components/molecules/DashboardFooter";
+import { useTranslation } from "next-i18next";
 import { useUserService } from "@services/user";
+import { useNavigateService } from "@services/navigate";
+import Modal from "@components/molecules/Modal";
 import DashboardButton from "@components/molecules/DashboardButton";
 import {
   Page,
@@ -14,6 +20,8 @@ import {
   Row,
   Col,
   Text,
+  Button,
+  Input,
   TableColumnDefinition,
   Table,
 } from "@jitera/jitera-web-ui-library";
@@ -22,21 +30,42 @@ type DashboardUsersPageProps = DefaultPageProps & {
   pageName?: string;
   className?: string;
 };
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Form1FormData {
+  input_userName: string;
+}
 function DashboardUsersPage(props: DashboardUsersPageProps): JSX.Element {
+  const { t } = useTranslation("web");
   const userService = useUserService();
   const getApiUsersInstance = userService.useGetApiUsers();
   const getApiUsersResult = getApiUsersInstance.useQuery();
+  const navigateService = useNavigateService();
+  const validationForm1Schema = useMemo(() => yup.object().shape({}), []);
+  const formForm1 = useForm<Form1FormData>({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    resolver: yupResolver(validationForm1Schema),
+    shouldFocusError: true,
+    mode: "onChange",
+    reValidateMode: "onChange",
+  });
+  const { errors: formForm1Errors } = formForm1.formState;
+
+  useEffect(() => {
+    formForm1.reset({});
+  }, []);
 
   const columnsTable1 = useMemo<TableColumnDefinition<any>[]>(
     () => [
+      { path: "id", name: "Id", sortable: false },
+      { path: "username", name: "Username", sortable: false },
+      { path: "email", name: "Email", sortable: false },
+      { path: "dateofbirth", name: "Dateofbirth", sortable: false },
       { path: "created_at", name: "Created At", sortable: false },
       { path: "updated_at", name: "Updated At", sortable: false },
       { path: "created_at", name: "Created At", sortable: false },
       { path: "updated_at", name: "Updated At", sortable: false },
       { path: "isactive", name: "Isactive", sortable: false },
-      { path: "username", name: "Username", sortable: false },
-      { path: "dateofbirth", name: "Dateofbirth", sortable: false },
-      { path: "email", name: "Email", sortable: false },
     ],
     []
   );
@@ -46,7 +75,7 @@ function DashboardUsersPage(props: DashboardUsersPageProps): JSX.Element {
       {
         name: "Delete",
         renderColumn: (props) => {
-          return <DashboardButton {...props.row.original} />;
+          return <Modal {...props.row.original} />;
         },
       },
       {
@@ -59,6 +88,18 @@ function DashboardUsersPage(props: DashboardUsersPageProps): JSX.Element {
     []
   );
 
+  const handleButton1 = async () => {
+    try {
+      navigateService.navigate("/newAdmin/dashboard/users/:userId");
+    } catch (e: unknown) {}
+  };
+  const handleButton0 = async (values?: Form1FormData) => {
+    try {
+      const responseGetApiUsers = await getApiUsersInstance.fetch({
+        users: { username: get(values, "input_userName", "") },
+      });
+    } catch (e: unknown) {}
+  };
   return (
     <Page className={styles.page_container}>
       <DashboardNavbar className={styles.dashboardnavbar_1} />
@@ -73,10 +114,46 @@ function DashboardUsersPage(props: DashboardUsersPageProps): JSX.Element {
             </Col>
             <Col md={Number(24)} xl={Number(18)} xs={Number(24)}>
               <Box className={styles.dashboard_content}>
-                <Box className={styles.dashboard_content_title}>
-                  <Text className={styles.text_9} textType="Text">
-                    Users
-                  </Text>
+                <Box className={styles.box_1}>
+                  <Box className={styles.box_3}>
+                    <Text className={styles.text_0} textType="Text">
+                      {t("dashboard_users.text_0")}
+                    </Text>
+                    <Button
+                      buttonType="primary"
+                      className={styles.button_1}
+                      onClick={handleButton1}
+                    >
+                      {t("dashboard_users.button_1")}
+                    </Button>
+                  </Box>
+                  <Box className={styles.form_1}>
+                    <Controller
+                      control={formForm1.control}
+                      render={({
+                        field: { onChange, onBlur, value },
+                        fieldState: { isTouched, error },
+                      }: any) => {
+                        return (
+                          <Input
+                            inputStyle={styles.input_user_name_input}
+                            placeholder={t("dashboardusers.input_1")}
+                            className={styles.input_user_name}
+                            onChange={onChange}
+                            value={value}
+                          />
+                        );
+                      }}
+                      name="input_userName"
+                    />
+                    <Button
+                      buttonType="primary"
+                      className={styles.button_0}
+                      onClick={formForm1.handleSubmit(handleButton0)}
+                    >
+                      Search
+                    </Button>
+                  </Box>
                 </Box>
                 <Box className={styles.dashboard_content_filter_table}>
                   <Box className={styles.box_8}>
